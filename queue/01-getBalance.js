719cf3c902fc1063cc0678730ac76c2ec5616b39
amin <amin.ziaei.4125@gmail.com>
2026-08-04 03:10:58 -0700

    chore: remove getBalance from queue

--- queue/01-getBalance.js
+++
@@ -1,9 +0,0 @@
-import { createPublicClient, http, formatEther } from 'viem';
-import { base } from 'viem/chains';
-
-const client = createPublicClient({ chain: base, transport: http() });
-
-export async function getBalance(address) {
-  const balance = await client.getBalance({ address });
-  return formatEther(balance);
-}
