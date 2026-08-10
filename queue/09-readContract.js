16ee7736296fa84e61da3546b47d1258a5622df5
amin <amin.ziaei.4125@gmail.com>
2026-08-10 03:23:35 -0700

    chore: remove readContract from queue

--- queue/09-readContract.js
+++
@@ -1,5 +0,0 @@
-import { publicClient } from './03-createPublicClient.js';
-
-export async function readTokenName(contractAddress, abi) {
-  return publicClient.readContract({ address: contractAddress, abi, functionName: 'name' });
-}
